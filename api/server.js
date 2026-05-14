63c0be0d3a954b824896767b490a527a3b2aef9c
ZGS <zgstowell@gmail.com>
2026-05-14 15:37:41 -0700

    directly serve html

--- api/server.js
+++ api/server.js
@@ -4,9 +4,11 @@ export default async function handler(req, res) {
     try {
         // Resolve the path to your HTML file
         const filePath = path.join(process.cwd(), 'public', 'index.html');
+        console.log({ filePath })
 
         // Read the file content
         const htmlContent = await fs.readFile(filePath, 'utf8');
+        console.log({ htmlContent })
 
         // Send as HTML response
         res.setHeader('Content-Type', 'text/html');
