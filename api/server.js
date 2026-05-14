264604de01989b011f62ec4a0d39c7fdb08e5964
ZGS <zgstowell@gmail.com>
2026-05-14 15:20:00 -0700

    server tests

--- api/server.js
+++ api/server.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = (req, res) => {
-    res.status(200).send("Hello from a standalone serverless function!");
+    console.log('Received request:', req.method, req.url);
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    res.statusCode = 200;
+    res.end("Hello from a standalone serverless function!");
 };
