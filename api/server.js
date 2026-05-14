f88f9b0c76b80efa589752944e29fff8bd3c16e8
ZGS <zgstowell@gmail.com>
2026-05-14 16:17:04 -0700

    vercel output?

--- api/server.js
+++ api/server.js
@@ -2,11 +2,25 @@ const fs = require('fs');
 const path = require('path');
 module.exports = (req, res) => {
     console.log('Received request:', req.method, req.url);
-    // Enable CORS
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    res.statusCode = 200;
-    res.end(`Hello from a standalone serverless function! You requested ${req.url} with method ${req.method}`);
+    const indexHtmlPath = path.join(process.cwd(), 'public', 'index.html');
+    fs.readFile(indexHtmlPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading index.html:', err);
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+            return;
+        }
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.end(data);
+    });
+
+    // // For testing API endpoint
+    // // Enable CORS
+    // res.setHeader('Access-Control-Allow-Credentials', 'true');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    // res.statusCode = 200;
+    // res.end(`Hello from a standalone serverless function! You requested ${req.url} with method ${req.method}`);
 };
